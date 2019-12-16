eb2f3cb24726325aeb7602ff37543132e7dabeff
Malick Mbengue <malickmbengue95@hotmail.fr>
2019-12-16 21:53:34 +0000

    crud du stock avec le service rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
@@ -10,7 +11,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ContentComponent } from './content/content.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app.routing.module';
-
+import { ProduitService } from './produit/produit.service';
 
 @NgModule({
   declarations: [
@@ -20,13 +21,16 @@ import { AppRoutingModule } from './app.routing.module';
     NavbarComponent,
     ContentComponent,
     DashboardComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
-  providers: [ProduitMockService],
+  providers: [ProduitMockService, ProduitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProduitComponent } from './produit/produit.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProduitResolver } from './produit/produit.resolver';
 
 export const appRoutes: Routes = [
-    { path: 'produit', component: ProduitComponent },
+    {
+        path: 'produit', component: ProduitComponent,
+        resolve: {
+            produits : ProduitResolver
+        }
+    },
     { path: 'dashboard', component: DashboardComponent },
     {path: '', redirectTo: '/dashbord', pathMatch: 'full'}
     
@@ -19,7 +25,9 @@ export const appRoutes: Routes = [
     ],
     exports: [
         RouterModule
-    ]
+    ],
+    providers : [ProduitResolver]
+    
 })
 
 export class AppRoutingModule {
